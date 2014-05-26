cea2e3e16381ff257da324b40664d4660ed262cc
Mike McDonald <mcdonamp@rose-hulman.edu>
2014-05-26 17:30:11 -0400

    Getting rid of code for the starter code

--- Firebase/Rose Chat/Starting Code/rosechat.js
+++ Firebase/Rose Chat/Starting Code/rosechat.js
@@ -1,21 +1,5 @@
-// Get a reference to the root of the chat data.
-var rootRef = new Firebase('https://rosechat.firebaseio.com');
-var messagesRef = rootRef.child('messages');
+// Get a reference to the messages child
 
 // When the user presses enter on the message input, write the message to firebase.
-$('#messageInput').keypress(function (e) {
-  if (e.keyCode == 13) {
-    var name = $('#nameInput').val();
-    var text = $('#messageInput').val();
-    messagesRef.push({name:name, text:text});
-    $('#messageInput').val('');
-  }
-});
 
 // Add a callback that is triggered for each chat message.
-messagesRef.limit(10).on('child_added', function (snapshot) {
-  var message = snapshot.val();
-  $('<div/>').text(message.text).prepend($('<em/>')
-    .text(message.name+': ')).appendTo($('#messagesDiv'));
-  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-});
