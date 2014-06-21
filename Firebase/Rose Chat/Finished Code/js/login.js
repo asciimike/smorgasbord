fe232fcf83e4399c2ed058407decd5bc2250ddd5
Mike McDonald <mcdonamp@rose-hulman.edu>
2014-06-20 22:15:25 -0700

    Clears password on failed login. Accepts return on password in addition to pressing the button.

--- Firebase/Rose Chat/Finished Code/js/login.js
+++ Firebase/Rose Chat/Finished Code/js/login.js
@@ -1,17 +1,29 @@
 var authRef = new Firebase('rosechat.firebaseio.com');
 var authClient = new FirebaseSimpleLogin(authRef, function(error, user) {
 	if (error) {
+		$('#passInput').val('');
 		alert(error.message);
 		return;
 	} else if (user) {
 		window.location.href = "rosechat.html";
 	} else {
 		$('#signInButton').on('click', function() {
-			console.log("Sign in button clicked");
-			authClient.login('password', {
-					email: $('#userInput').val(),
-					password: $('#passInput').val()
-					});
-			});
+			userLogIn($('#userInput').val(),$('#passInput').val())
+		});
 	}
+});
+
+var userLogIn = function(username, password){
+	authClient.login('password', {
+					email: username,
+					password: password
+					});
+}
+
+$(function(){
+	$('#passInput').keypress(function (event) {
+    	if (event.keyCode == 13) {
+    		userLogIn($('#userInput').val(),$('#passInput').val())
+    	}
+  	});
 });
